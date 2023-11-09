50e509f96e913b746b0a30a307cdfd0cf9bd7560
Meet Patel <149522835+meet1410001@users.noreply.github.com>
2023-11-10 02:18:23 +0530

    Update views.py

--- ITC/ITCapp/views.py
+++ ITC/ITCapp/views.py
@@ -1,63 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import *
-
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
 
 def login(request):
     if request.method == 'POST':
+        us = request.POST['username']
+        pw = request.POST['password']
 
-        if request.POST['loginbtn']:
+        user = authenticate(request, username=us, password=pw)
 
-            print("Login!!")
-            us = request.POST['user']
-            pw = request.POST['pw']
-
-            try:
-                record = user.objects.get(username=us)
-                dpw = record.password
-
-                if pw == dpw:
-                    return render(request, 'index.html')
-                else:
-                    print("Password Incorrect!!")
-                    return render(request, 'login.html')
-
-            except:
-                print("User not found!!")
+        if user is not None:
+            if check_password(pw, user.password):  # Use check_password to validate the password
+                auth_login(request, user)
+                return render(request, 'index.html')
+            else:
+                print("Password Incorrect!!")
                 return render(request, 'login.html')
-
         else:
+            print("User not found!!")
             return render(request, 'login.html')
 
-    else:
-        return render(request, 'login.html')
-
+    return render(request, 'login.html')
 
 def registration(request):
-
     if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
 
-        if request.POST['register']:
-            us = request.POST['user']
-            pw = request.POST['pw']
-            name = request.POST['name']
-            email = request.POST['email']
-            age = request.POST['age']
-            city = request.POST['city']
-            state = request.POST['state']
-            country = request.POST['country']
-
-            new_user = user(username=us, password=pw, name=name, email=email, age=age, city=city, state=state, country=country)
-            new_user.save()
-
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.password = make_password(form.cleaned_data['password'])  # Hash the password
+            user.save()
             return render(request, 'login.html')
-
-        else:
-            return render(request, 'register.html')
-
     else:
-        return render(request, 'register.html')
+        form = UserRegistrationForm()
 
+    return render(request, 'register.html', {'form': form})
 
+# def index(request):
+#     return render(request, 'index.html')
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+@login_required(login_url='login')  # Decorator to ensure the user is logged in
 def index(request):
-    return render(request, 'index.html')
+    current_username = request.user.username
+    all_usernames = User.objects.values_list('username', flat=True)
+
+    return render(request, 'index.html', {'current_username': current_username, 'all_usernames': all_usernames})
